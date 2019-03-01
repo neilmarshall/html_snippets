5338a1f6d865d74be04156180089ba73543285a9
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-03-01 14:00:46 +0000

    further tests for flask_sqlalchemy example

--- flask_sql_alchemy_example/app/models.py
+++ flask_sql_alchemy_example/app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 class League(db.Model):
 
@@ -10,15 +12,20 @@ class League(db.Model):
 
 class Match(db.Model):
 
-    league_id = db.Column(db.Integer, primary_key=True, )
+    league_id = db.Column(db.Integer, ForeignKey(League.id), primary_key=True)
     season = db.Column(db.Text, primary_key=True)
+
+    leagues = relationship("League", backref="Match")
 
     def __repr__(self):
         return f"Match(league_id={self.league_id}, season={self.season})"
 
 
 def get_seasons_by_league(league):
-    return [season for (season,) in db.session.query(Match.season)
-                                              .filter(League.id==Match.league_id)
-                                              .filter(League.name==league)
-                                              .distinct()]
+    return [match.season for match in db.session.query(Match.season)
+                                                .filter(League.id==Match.league_id)
+                                                .filter(League.name==league)
+                                                .distinct()]
+
+def get_all_leagues():
+    return [league.name for league in db.session.query(League.name).order_by(League.name).distinct()]
--- flask_sql_alchemy_example/tests/test_models.py
+++ flask_sql_alchemy_example/tests/test_models.py
@@ -1,6 +1,6 @@
 import unittest
 from app import app, db
-from app.models import League, Match, get_seasons_by_league
+from app.models import League, Match, get_seasons_by_league, get_all_leagues
 
 class TestDBFunctions(unittest.TestCase):
 
@@ -13,7 +13,9 @@ class TestDBFunctions(unittest.TestCase):
         db.session.add_all(
             [League(id=1729, name="England Premier League"),
              League(id=4769, name="France Ligue 1"),
-             League(id=10257, name="Italy Serie A")])
+             League(id=10257, name="Italy Serie A"),
+             League(id=8257, name="Italy Serie A"),
+             League(id=17809, name="Germany 1. Bundesliga")])
         db.session.add_all(
              [Match(league_id=1729, season="2008/2009"),
               Match(league_id=1729, season="2009/2010"),
@@ -37,6 +39,7 @@ class TestDBFunctions(unittest.TestCase):
         db.session.commit()
 
     def tearDown(self):
+        db.drop_all()
         app.config['SQLALCHEMY_DATABASE_URI'] = self.app_config
  
     def test_get_seasons_by_league(self):
@@ -46,6 +49,10 @@ class TestDBFunctions(unittest.TestCase):
         self.assertEqual(seasons, ["2010/2011", "2011/2012", "2012/2013", "2013/2014", "2014/2015", "2015/2016"])
         seasons = get_seasons_by_league("Italy Serie A")
         self.assertEqual(seasons, ["2008/2009", "2009/2010", "2010/2011", "2011/2012", "2012/2013"])
+
+    def test_get_all_leagues(self):
+        countries = get_all_leagues()
+        self.assertEqual(countries, ["England Premier League", "France Ligue 1", "Germany 1. Bundesliga", "Italy Serie A"])
  
  
 if __name__ == '__main__':
