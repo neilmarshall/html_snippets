e6e36555eb5e63dadd6168c94c7b71ae46793b26
neilmarshall <neil.marshall@dunelm.org.uk>
2019-03-02 19:01:38 +0000

    feat: update flask_sqlalchemy example to use GET in place of POST styling

--- flask_sql_alchemy_example/app/routes.py
+++ flask_sql_alchemy_example/app/routes.py
@@ -3,17 +3,17 @@ from flask import render_template, request
 from app import app
 import app.models as models
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/index')
 def index():
     leagues = models.get_all_leagues()
     seasons = json.dumps({league: models.get_seasons_by_league(league) for league in leagues})
     results = None
     selected_league = None
     selected_season = None
-    if request.method == 'POST':
-        selected_league = request.form['leagues_selector']
-        selected_season = request.form['seasons_selector']
+    if request.args:
+        selected_league = request.args['leagues_selector']
+        selected_season = request.args['seasons_selector']
         results = models.ResultsAggregator(models.get_matches(selected_league,
             selected_season))
     return render_template('index.html', leagues=leagues, seasons=seasons,
