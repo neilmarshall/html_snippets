9a91ec3d766a6fec7e31fea0d89ab094e0a68c1d
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-04-03 17:33:37 +0100

    feat: Update HTTP auth'n to test against encrypted passwords

--- flask_auth/basic_http/app/resources/foo.py
+++ flask_auth/basic_http/app/resources/foo.py
@@ -1,14 +1,17 @@
+import hashlib
+
 from flask_httpauth import HTTPBasicAuth
 from flask_restful import Resource
 
 auth = HTTPBasicAuth()
 
-users = {'test_user': 'pass'}
+users = {'test_user': 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1'}  # sha256 encoding of 'pass'
 
-@auth.get_password
-def get_pw(username):
+@auth.verify_password
+def verify_pw(username, password):
     if username in users:
-        return users.get(username)
+        hashed_password = hashlib.sha256(bytes(password, 'utf-8')).hexdigest()
+        return users.get(username) == hashed_password
     return None
 
 
