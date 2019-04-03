f129264f87807488be959d86b2f52c73fa5e72c3
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-04-03 14:29:21 +0100

    refactor: Use Flask HTTPBasicAuth

--- flask_auth/basic_http/app/resources/foo.py
+++ flask_auth/basic_http/app/resources/foo.py
@@ -1,7 +1,18 @@
-from flask_login import login_required
+from flask_httpauth import HTTPBasicAuth
 from flask_restful import Resource
 
+auth = HTTPBasicAuth()
+
+users = {'test_user': 'pass'}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+
 class Foo(Resource):
-    @login_required
+    @auth.login_required
     def get(self):
         return {"content": "top secret information"}, 200
--- flask_auth/basic_http/config.py
+++ flask_auth/basic_http/config.py
@@ -6,5 +6,3 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
