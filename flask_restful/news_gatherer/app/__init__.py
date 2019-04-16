91f53b306cb09f209b71fc8fbcc2c9c934ab7ff9
neilmarshall <neil.marshall@dunelm.org.uk>
2019-04-16 22:11:50 +0100

    feat: add database migration

--- flask_restful/news_gatherer/app/__init__.py
+++ flask_restful/news_gatherer/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 from app.resources.news import GetNews, GetToken, Register
 
@@ -11,6 +13,7 @@ def create_app(config_object):
     app.config.from_object(config_object)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     api = Api(app)
     api.add_resource(GetNews, '/get_news')
--- flask_restful/news_gatherer/config.py
+++ flask_restful/news_gatherer/config.py
@@ -6,5 +6,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_SCHEME') + \
+            os.path.abspath(os.environ.get('SQLALCHEMY_DATABASE_RELATIVE_PATH'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
