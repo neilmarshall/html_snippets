41a30763e9167b0d452b4d237b8ce2e075e05850
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-04-16 09:49:12 +0100

    feat: add flask shell context processor

--- flask_restful/news_gatherer/run.py
+++ flask_restful/news_gatherer/run.py
@@ -1,4 +1,9 @@
-from app import create_app
+from app import create_app, db
+from app.resources.news import User
 from config import Config
 
 app = create_app(Config)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
