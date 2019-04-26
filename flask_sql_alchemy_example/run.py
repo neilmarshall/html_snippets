06376e680fb87eeacdebc8a5f6da1737efedf4a6
neilmarshall <neil.marshall@dunelm.org.uk>
2019-04-26 23:10:52 +0100

    add make_shell_context for flask_sqlalchemy example

--- flask_sql_alchemy_example/run.py
+++ flask_sql_alchemy_example/run.py
@@ -1,2 +1,10 @@
-from app import create_app
+from app import create_app, db
+from app.models import *
+
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'League': League, 'Team': Team, 'Match': Match,
+            'get_all_leagues': get_all_leagues, 'get_seasons_by_league': get_seasons_by_league,
+            'get_matches': get_matches, 'ResultsAggregator': ResultsAggregator}
